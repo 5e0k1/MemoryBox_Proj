1ffd58b32c1c385e17cdb37d74091ea158878725
5e0k1 <ksc03073@gmail.com>
2026-04-07 22:36:07 +0900

    Feat : Create MemoryBox Project(SpringBoot).

---
+++ MemoryBox/src/test/java/com/hogudeul/demo/MemoryBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hogudeul.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
