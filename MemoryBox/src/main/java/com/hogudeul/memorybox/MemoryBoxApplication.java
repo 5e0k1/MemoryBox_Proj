41bd0b55bab1ad6c19b194625aa17ec4898bda88
5e0k1 <ksc03073@gmail.com>
2026-04-10 08:09:27 +0900

    Chore : Wrong Package Fixed, gitignore added

--- MemoryBox/src/main/java/com/hogudeul/memorybox/MemoryBoxApplication.java
+++ MemoryBox/src/main/java/com/hogudeul/memorybox/MemoryBoxApplication.java
@@ -2,7 +2,7 @@ package com.hogudeul.memorybox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MemoryBoxApplication {
