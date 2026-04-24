772d2d4319c0ef86d0cb856c114ad675339e6de7
5e0k1 <ksc03073@gmail.com>
2026-04-25 08:45:30 +0900

    Fix all-day ICS date shift by normalizing to Korea timezone

---
+++ MemoryBox/src/main/java/com/hogudeul/memorybox/dto/calendar/CalendarEventDto.java
@@ -0,0 +1,58 @@
+package com.hogudeul.memorybox.dto.calendar;
+
+public class CalendarEventDto {
+    private String title;
+    private String date;
+    private String timeText;
+    private String sourceName;
+    private String sourceType;
+    private boolean allDay;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTimeText() {
+        return timeText;
+    }
+
+    public void setTimeText(String timeText) {
+        this.timeText = timeText;
+    }
+
+    public String getSourceName() {
+        return sourceName;
+    }
+
+    public void setSourceName(String sourceName) {
+        this.sourceName = sourceName;
+    }
+
+    public String getSourceType() {
+        return sourceType;
+    }
+
+    public void setSourceType(String sourceType) {
+        this.sourceType = sourceType;
+    }
+
+    public boolean isAllDay() {
+        return allDay;
+    }
+
+    public void setAllDay(boolean allDay) {
+        this.allDay = allDay;
+    }
+}
