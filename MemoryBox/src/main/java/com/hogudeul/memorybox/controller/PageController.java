54299021b62e192229520964427b2c59a057e69a
5e0k1 <ksc03073@gmail.com>
2026-04-30 03:21:24 +0900

    Add missing HttpStatus import in PageController

--- MemoryBox/src/main/java/com/hogudeul/memorybox/controller/PageController.java
+++ MemoryBox/src/main/java/com/hogudeul/memorybox/controller/PageController.java
@@ -33,6 +33,7 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.core.io.FileSystemResource;
